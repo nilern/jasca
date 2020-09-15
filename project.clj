29455b60e02b30a0f17774a96166e59ac260f721
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2020-09-15 15:37:37 +0300

    Initial commit; create project.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject jasca "0.1.0-SNAPSHOT"
+  :description "JavAScript object notation parser CombinAtors"
+  :url "https://github.com/nilern/jasca"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns jasca.core})
