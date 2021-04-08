a926cf6415dda3d19956ea00dc353d78d70052b5
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-04-08 19:15:41 +0300

    Add Malli decoder options to custom decoder perf test

--- test/jasca/custom_decode_perf_test.clj
+++ test/jasca/custom_decode_perf_test.clj
@@ -5,9 +5,30 @@
             [jasca.impl.grammar :as impl]
             [jasca.generic :as generic]
             [malli.core :as m]
+            [malli.transform :as mt]
             [criterium.core :as cc])
   (:import [java.time LocalDateTime]
-           [java.time.format DateTimeFormatter]))
+           [java.time.format DateTimeFormatter DateTimeParseException]))
+
+(defn- parse-hex-string [^String s] (bigint (BigInteger. s 16)))
+
+(defn- hex-string-decoder [s]
+  (if (string? s)
+    (try
+      (parse-hex-string s)
+      (catch NumberFormatException _ s))
+    s))
+
+(def ^:private parse-randomuser-instant
+  (let [formatter (DateTimeFormatter/ofPattern "uuuu-MM-dd HH:mm:ss")]
+    (fn [s] (LocalDateTime/parse s formatter))))
+
+(defn- local-date-time-decoder [s]
+  (if (string? s)
+    (try
+      (parse-randomuser-instant s)
+      (catch DateTimeParseException _ s))
+    s))
 
 (def ^:private LocalDatetime
   (m/-simple-schema {:type :local-date-time, :pred #(instance? LocalDateTime %)}))
@@ -22,9 +43,9 @@
   (m/schema
     [:map {:closed true}
      [:username :string]
-     [:md5 integer?]
-     [:sha1 integer?]
-     [:sha256 integer?]]))
+     [:md5 {:decode/randomuser hex-string-decoder} integer?]
+     [:sha1 {:decode/randomuser hex-string-decoder} integer?]
+     [:sha256 {:decode/randomuser hex-string-decoder} integer?]]))
 
 (def ^:private UserName
   (m/schema
@@ -39,7 +60,7 @@
      [:street :string]
      [:city :string]
      [:state :string]
-     [:postcode :string]]))
+     [:postcode {:decode/randomuser str} :string]]))
 
 (def ^:private User
   (m/schema
@@ -49,24 +70,24 @@
      [:name UserName]
      [:nat :string]
      [:login Login]
-     [:dob LocalDatetime]
+     [:dob {:decode/randomuser local-date-time-decoder} LocalDatetime]
      [:id UserId]
      [:picture [:map {:closed true}
                 [:large :string]
                 [:medium :string]
                 [:thumbnail :string]]]
      [:gender :string]
-     [:registered LocalDatetime]
+     [:registered {:decode/randomuser local-date-time-decoder} LocalDatetime]
      [:cell :string]
      [:location Location]]))
 
 (def ^:private validate-users (m/validator [:vector User]))
 
-(defn- parse-hex-string [^String s] (bigint (BigInteger. s 16)))
-
-(def ^:private parse-randomuser-instant
-  (let [formatter (DateTimeFormatter/ofPattern "uuuu-MM-dd HH:mm:ss")]
-    (fn [s] (LocalDateTime/parse s formatter))))
+(def ^:private malli-decode-users
+  (m/decoder [:vector User]
+             (mt/transformer
+               mt/strip-extra-keys-transformer
+               {:name :randomuser})))
 
 (defn- postprocess-location [location] (update location :postcode str))
 
@@ -91,6 +112,13 @@
     (assert (validate-users users))
     users))
 
+(defn- decode-cheshire-malli [s]
+  (let [users (-> (cheshire/parse-string s true)
+                  :results
+                  malli-decode-users)]
+    (assert (validate-users users))
+    users))
+
 (defn- decode-jsonista [s]
   (let [users (->> (j/read-value s j/keyword-keys-object-mapper)
                    :results
@@ -98,55 +126,63 @@
     (assert (validate-users users))
     users))
 
-(def ^:private decode-jasca
-  (let [grammar (assoc generic/grammar
-                  :hex-hash [:-> String parse-hex-string]
-                  :randomuser-instant [:-> String parse-randomuser-instant]
+(defn- decode-jsonista-malli [s]
+  (let [users (-> (j/read-value s j/keyword-keys-object-mapper)
+                  :results
+                  malli-decode-users)]
+    (assert (validate-users users))
+    users))
 
-                  :user-id [:object keyword {:name String
-                                             :value [:or String nil]}]
+(def ^:private grammar
+  (assoc generic/grammar
+    :hex-hash [:-> String parse-hex-string]
+    :randomuser-instant [:-> String parse-randomuser-instant]
 
-                  :login [:-> [:object keyword
-                               {:username String
-                                :password String
-                                :salt String
-                                :md5 :hex-hash
-                                :sha1 :hex-hash
-                                :sha256 :hex-hash}]
-                          #(dissoc % :password :salt)]
+    :user-id [:object keyword {:name String
+                               :value [:or String nil]}]
 
-                  :user-name [:object keyword
-                              {:title String
-                               :first String
-                               :last String}]
+    :login [:-> [:object keyword
+                 {:username String
+                  :password String
+                  :salt String
+                  :md5 :hex-hash
+                  :sha1 :hex-hash
+                  :sha256 :hex-hash}]
+            #(dissoc % :password :salt)]
 
-                  :location [:object keyword
-                             {:street String
-                              :city String
-                              :state String
-                              :postcode [:or String [:-> Long str]]}]
+    :user-name [:object keyword
+                {:title String
+                 :first String
+                 :last String}]
 
-                  :user [:object keyword
-                         {:id :user-id
-                          :login :login
-                          :name :user-name
-                          :gender String
-                          :dob :randomuser-instant
-                          :registered :randomuser-instant
-                          :nat String
-                          :location :location
-                          :email String
-                          :phone String
-                          :cell String
-                          :picture [:object keyword
-                                    {:large String
-                                     :medium String
-                                     :thumbnail String}]}]
+    :location [:object keyword
+               {:street String
+                :city String
+                :state String
+                :postcode [:or String [:-> Long str]]}]
 
-                  :response [:-> [:object {"results" [:array-of :user]
-                                           "info" :value}]  ; TODO: :skip-value
-                             #(get % "results")])]
-    (-> grammar (impl/parsers [:response]) first)))
+    :user [:object keyword
+           {:id :user-id
+            :login :login
+            :name :user-name
+            :gender String
+            :dob :randomuser-instant
+            :registered :randomuser-instant
+            :nat String
+            :location :location
+            :email String
+            :phone String
+            :cell String
+            :picture [:object keyword
+                      {:large String
+                       :medium String
+                       :thumbnail String}]}]
+
+    :response [:-> [:object {"results" [:array-of :user]
+                             "info" :value}]  ; TODO: :skip-value
+               #(get % "results")]))
+
+(def ^:private decode-jasca (-> grammar (impl/parsers [:response]) first))
 
 (defn decode-perf-size [size]
   (let [file (str "dev-resources/json" size ".json")
@@ -158,8 +194,14 @@
     (println "custom decode: cheshire")
     (cc/quick-bench (decode-cheshire json))
 
+    (println "custom decode: cheshire + malli")
+    (cc/quick-bench (decode-cheshire-malli json))
+
     (println "custom decode: jsonista")
     (cc/quick-bench (decode-jsonista json))
+
+    (println "custom decode: jsonista + malli")
+    (cc/quick-bench (decode-jsonista-malli json))
 
     (println "custom decode: jasca")
     (cc/quick-bench (decode-jasca (.createParser generic/+factory+ json)))))
